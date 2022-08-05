0c0a1dbfb015d91092f90d75129e198ef4b25f1b
Support <Support@DESKTOP-SE5ODLJ>
2022-08-05 03:39:20 +0100

    Add project files.

---
+++ Hospital.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment0003
+{
+    public interface IHospital
+    {
+        public int PatientNum { get; set; }
+        public string PatientBioData { get; set; }
+        public string PatientNextofKin { get; set; }
+
+        public bool PayingHMO(string hmoName);
+
+        public bool PatientSerivce(string status);
+        public int PaymnetDebt(string debt);
+
+
+    }
+}
